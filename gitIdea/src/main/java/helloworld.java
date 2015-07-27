255365f921a85ba7817dd86d55ec82e56918ec08
Shoo <eric@sjtu.edu.cn>
2015-07-27 12:37:00 +0800

    java class hello world from idea

---
+++ gitIdea/src/main/java/helloworld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Shoo on 2015/7/27.
+ */
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello world from intellji idea!");
+    }
+}
